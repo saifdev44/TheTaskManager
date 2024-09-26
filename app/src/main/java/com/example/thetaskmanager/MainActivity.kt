98621fea6a0e7fcda51439ef488f75916585515b
saifdev44 <saifdev44@gmail.com>
2024-09-26 13:01:57 +0600

    My first commit for The Tak Manager

---
+++ app/src/main/java/com/example/thetaskmanager/MainActivity.kt
@@ -0,0 +1,79 @@
+package com.example.thetaskmanager
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.thetaskmanager.ui.theme.TheTaskManagerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TheTaskManagerTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    TaskCompleteScreen(
+                        messageOne = stringResource(R.string.all_task_completed),
+                        messageTwo = stringResource(R.string.nice_work),
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TaskCompleteScreen(messageOne: String, messageTwo: String, modifier: Modifier = Modifier){
+    val image = painterResource(R.drawable.ic_task_completed)
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = modifier.width(100.dp)
+        )
+
+        Text(
+            text = messageOne,
+            fontWeight = FontWeight.Bold,
+            fontSize = 24.sp
+        )
+
+        Text(
+            text = messageTwo,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TheTaskManagerTheme {
+        TaskCompleteScreen(
+            messageOne = stringResource(R.string.all_task_completed),
+            messageTwo = stringResource(R.string.nice_work)
+        )
+    }
+}
